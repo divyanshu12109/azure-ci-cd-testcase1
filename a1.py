f974b8a1c8b7da257929ccca1de400d20b50f14e
lord-divyanshu <111170351+lord-divyanshu@users.noreply.github.com>
2022-08-13 13:27:46 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,7 +1,19 @@
-def fact(a):
-    b=1
-    while a>1:
-        b=a*b
-        a=a-1
-    print(b)
+def pypart(n):
+     
+    # outer loop to handle number of rows
+    # n in this case
+    for i in range(0, n):
+     
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
+         
+            # printing stars
+            print("* ",end="")
+      
+        # ending line after each row
+        print("\r")
  
+# Driver Code
+n = 5
+pypart(n)
